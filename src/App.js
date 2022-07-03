88dd628e09aeaea549d322e7adfa1da7ebbf80e0
Maha Mamoor <mamoorm@gmail.com>
2022-07-02 22:14:32 -0400

    added a dropdown describing what the app can be used for

--- src/App.js
+++ src/App.js
@@ -44,24 +44,33 @@ const App = () => {
   }, [])
 
   return (
-    <div className="bg-sky-700">
-      <div className='brands'>
-      <h1 className="text-3xl font-bold underline hover:bg-black hover:text-white">Inventory App</h1>
-      <h2 className="text-2xl font-bold hover:bg-black hover:text-white">Raw Materials</h2>
-      <div className="searchbar">
+    <div className="">
+      <div className="header">
+        <h1 className="text-3xl text-center font-bold hover:bg-black hover:text-white">Inventory App</h1>
+        <h2 className="text-2xl text-center font-bold hover:bg-black hover:text-white">Raw Materials</h2>
+      </div>
+        <div className="max-w-lg mx-auto p-8">
+          <details className="open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg" open>
+          <summary className="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">What function does this app serve?</summary>
+          <div className="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+            <p>The purpose of this app is to track your inventory in all of your warehouses.</p>
+          </div>
+          </details>
+        </div>
+      <div className="text-center">
         <p className="productSearch">Search for your product by Supplier</p>
         <input className="input-search" type="text" placeholder="Search..." onChange={event => {setSearch(event.target.value)}}/>
       </div>
-      <table className="table-fixed">
+      <table className="w-full">
         <thead>
-          <tr className="odd:bg-white even:bg-slate-50">
-            <th>Name</th>
-            <th>Product</th>
-            <th>Warehouse</th>
-            <th>Status</th>
-            <th>Quantity</th>
-            <th>Edit Product</th>
-            <th>Delete Inventory</th>
+          <tr className="bg-gray-50 border-b-2 border-gray-200">
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Name</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Product</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Warehouse</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Status</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Quantity</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Edit Product</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Delete Inventory</th>
           </tr>
         </thead>
         <div className="products-list">
@@ -74,7 +83,7 @@ const App = () => {
         }).map((brand) => {
         return(
           <>
-            <tbody>
+            <tbody className="">
               <tr className="odd:bg-white even:bg-slate-50"  key={brand.id}>
                 <td>{brand.name}</td>
                 <td>{brand.product}</td>
@@ -94,7 +103,6 @@ const App = () => {
       </table>
       <h5 className="text-2xl font-bold">Add Product to Inventory</h5>
         <Add handleCreate={handleCreate}/>
-      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+     },
+     borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
 }
